1f88650f9a896dda43782d33087c36e0f19cc030
Tobias Ortmayr <Tobias Ortmayr>
2016-05-09 21:35:33 +0200

    Added reset of already finsihed activity diagrams once they are called again

--- org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/service/DecoratorService.java
+++ org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/service/DecoratorService.java
@@ -2,26 +2,31 @@ package org.modelexecution.xmof.animation.decorator.service;
 
 import org.eclipse.graphiti.tb.IDecorator;
 
-
 public class DecoratorService {
 
 	private static Object decoratedElement;
 	private static IDecorator[] decorators;
 
-	public static void setDecoratedElement(Object element, IDecorator[] decorators) {
-		DecoratorService.decorators=decorators;
+	public static void setDecoratedElement(Object element,
+			IDecorator[] decorators) {
+		DecoratorService.decorators = decorators;
 		decoratedElement = element;
 	}
-	
+
 	public static IDecorator[] getDecorators() {
 		return decorators;
 	}
-
 
 	public static boolean equalsDecoratedElement(Object element) {
 		if (decoratedElement == null)
 			return false;
 		return decoratedElement.equals(element);
+	}
+
+	public static void clear() {
+		decoratedElement = null;
+		decorators = null;
+
 	}
 
 }
