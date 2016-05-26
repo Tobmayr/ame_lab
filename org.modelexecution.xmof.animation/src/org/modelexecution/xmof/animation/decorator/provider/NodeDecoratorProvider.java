b54309a07b0fbd89672916fda53f7554a1523090
Tobias Ortmayr <Tobias Ortmayr>
2016-05-26 23:23:51 +0200

    added necessary java extension in odesign file

--- org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/provider/NodeDecoratorProvider.java
+++ org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/provider/NodeDecoratorProvider.java
@@ -3,7 +3,7 @@ package org.modelexecution.xmof.animation.decorator.provider;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.tb.IDecorator;
-import org.modelexecution.xmof.animation.decorator.service.DecoratorService;
+import org.modelexecution.xmof.animation.decorator.service.GraphitiDecoratorService;
 import org.modelexecution.xmof.diagram.decoration.IXMOFDecoratorProvider;
 
 public class NodeDecoratorProvider implements IXMOFDecoratorProvider {
@@ -12,8 +12,8 @@ public class NodeDecoratorProvider implements IXMOFDecoratorProvider {
 	public IDecorator[] getDecorators(PictogramElement pe) {
 
 		for (EObject businessObject : pe.getLink().getBusinessObjects()) {
-			if (DecoratorService.equalsDecoratedElement(businessObject)) {
-				return DecoratorService.getDecorators();
+			if (GraphitiDecoratorService.equalsDecoratedElement(businessObject)) {
+				return GraphitiDecoratorService.getDecorators();
 			}
 
 		}
