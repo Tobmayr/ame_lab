6c3a92651728bfcc561ad9c64b8bae29dd4ea501
Tobias Ortmayr <Tobias Ortmayr>
2016-06-28 03:36:58 +0200

    EdgeDecoration Top-Level implementation

--- dev/language_workbench/org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/SiriusDiagramDecorator.java
+++ dev/language_workbench/org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/SiriusDiagramDecorator.java
@@ -3,6 +3,7 @@ package org.modelexecution.xmof.animation.decorator;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EObject;
 import org.modelexecution.xmof.Syntax.Activities.ExtraStructuredActivities.impl.ExpansionRegionImpl;
 import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.Activity;
 import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.ActivityNode;
@@ -20,7 +21,7 @@ public class SiriusDiagramDecorator extends DiagramDecorator {
 	}
 
 	private void intializeActivityNodesSet(Activity activity) {
-		activityNodes= new HashSet<>();
+		activityNodes = new HashSet<>();
 		for (ActivityNode node : activity.getNode()) {
 			processActivityNode(node);
 		}
@@ -46,41 +47,31 @@ public class SiriusDiagramDecorator extends DiagramDecorator {
 	}
 
 	@Override
-	public boolean decorateActivityNode(Match match) {
-	 if (isActivityFinished()){
-		
-		setActivityFinished(false);
-	 }
-	 
-	 if (activityNodes.contains(match.getXmofElementName().trim())){
-		 SiriusDecoratorService.setActiveElement(match, getActivity().getName());
-		 return true;
-	 }
-	 
-	 return false;
-	
-	}
-	
+	public boolean decorateActivityElement(Match match) {
+		if (isActivityFinished()) {
 
-	
+			setActivityFinished(false);
+		}
+
+		if (activityNodes.contains(match.getXmofElementName().trim())) {
+			SiriusDecoratorService.setActiveElement(match, getActivity().getName());
+			return true;
+		}
+
+		return false;
+
+	}
 
 	@Override
 	public void resetDecorations() {
 		SiriusDecoratorService.clear();
-		
+
 	}
 
 	@Override
-	protected void decoratePreviouslyActiveNode(DecorationType nodeTraversed) {
+	protected void decorateElement(EObject element, DecorationType type) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	protected void decorateActiveNode(DecorationType structuredNodeActive) {
-		// TODO Auto-generated method stub
-		
 	}
-
 
 }
