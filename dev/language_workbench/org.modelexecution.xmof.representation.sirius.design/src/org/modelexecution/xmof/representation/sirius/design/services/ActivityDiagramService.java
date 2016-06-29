973fa359cfdcf38624833caf036a059c5d370262
Tobmayr <tobmayr@hotmail.com>
2016-06-29 11:45:29 +0200

    Create tools for ControlNodes

---
+++ dev/language_workbench/org.modelexecution.xmof.representation.sirius.design/src/org/modelexecution/xmof/representation/sirius/design/services/ActivityDiagramService.java
@@ -0,0 +1,23 @@
+package org.modelexecution.xmof.representation.sirius.design.services;
+
+import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.Activity;
+import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.ActivityNode;
+
+public class ActivityDiagramService {
+
+	public static String generateLabel(Activity activity) {
+		String label = activity.getName();
+		char first = Character.toUpperCase(label.charAt(0));
+		return first + label.substring(1);
+
+	}
+
+	public static String generateLabel(ActivityNode node) {
+		return node.eClass().getName();
+	}
+
+	public static String computeDefaultName(ActivityNode node) {
+		return LabelServices.INSTANCE.computeDefaultName(node);
+	}
+
+}
---
+++ dev/language_workbench/org.modelexecution.xmof.representation.sirius.design/src/org/modelexecution/xmof/representation/sirius/design/services/LabelServices.java
@@ -0,0 +1,35 @@
+package org.modelexecution.xmof.representation.sirius.design.services;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.ActivityNode;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+public class LabelServices {
+	public static final LabelServices INSTANCE = new LabelServices();
+
+	private LabelServices() {
+
+	}
+
+	public String computeDefaultName(final EObject element) {
+
+		Predicate<EObject> predicate = null;
+		String name = element.getClass().getSimpleName();
+		name = name.substring(0, name.indexOf("Impl"));
+		predicate = new Predicate<EObject>() {
+			public boolean apply(EObject input) {
+				return input.getClass().getName().equals(element.getClass().getName());
+			}
+		};
+		final List<EObject> existingElements = Lists
+				.newArrayList(Iterables.filter(element.eContainer().eContents(), predicate));
+
+		return name + existingElements.size();
+	}
+
+}
