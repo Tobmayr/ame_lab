7bf1ea497de54a8422d6d6c857c6cd37dabddc0f
Tobias Ortmayr <Tobias Ortmayr>
2016-06-30 16:06:46 +0200

    refactored GraphitiDecorationService and SiriusDecorationService into one class

--- dev/language_workbench/org.modelexecution.xmof.animation/src/org/modelexecution/xmof/animation/decorator/DecorationType.java
+++
@@ -1,7 +0,0 @@
-package org.modelexecution.xmof.animation.decorator;
-
-public enum DecorationType {
-
-	NODE_ACTIVE, NODE_TRAVERSED, STRUCTURED_NODE_ACTIVE, STRUCTURED_NODE_TRAVERSED, UNDECORATED_NODE, EDGE_ACTIVE, EDGE_TRAVERSED;
-
-}
