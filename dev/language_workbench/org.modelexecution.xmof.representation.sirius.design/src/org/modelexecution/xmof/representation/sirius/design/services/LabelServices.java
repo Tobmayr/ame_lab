738ef6d3bb3f129d979045932d4e62fb73c9c30e
Tobias Ortmayr <t.ortmayr@outlook.com>
2016-07-12 23:55:42 +0200

    Creation tool for InputPins started

--- dev/language_workbench/org.modelexecution.xmof.representation.sirius.design/src/org/modelexecution/xmof/representation/sirius/design/services/LabelServices.java
+++ dev/language_workbench/org.modelexecution.xmof.representation.sirius.design/src/org/modelexecution/xmof/representation/sirius/design/services/LabelServices.java
@@ -3,6 +3,9 @@ package org.modelexecution.xmof.representation.sirius.design.services;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
+import org.modelexecution.xmof.Syntax.Actions.BasicActions.Action;
+import org.modelexecution.xmof.Syntax.Actions.BasicActions.Pin;
+import org.modelexecution.xmof.Syntax.Actions.IntermediateActions.LinkAction;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -31,4 +34,18 @@ public class LabelServices {
 		return name + existingElements.size();
 	}
 
+	public String computeLabel(final Pin pin){
+		if (pin.eContainer() instanceof LinkAction){
+			return computeInputLabel((LinkAction) pin.eContainer() );
+		}
+		return "";
+	}
+	
+	private String computeInputLabel(LinkAction action){
+		String label="inputValue";
+		if (action.getInputValue()!=null){
+			label+=action.getInputValue().size();
+		}
+		return label;
+	}
 }
